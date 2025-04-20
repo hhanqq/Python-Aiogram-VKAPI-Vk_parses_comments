87653fe85b45a9e7abd4ed60c8ee3e780ac63d42
hhanqq <sdudnik05@mail.ru>
2025-04-20 17:31:52 +0700

    make url good

--- output.py
+++ output.py
@@ -4,10 +4,10 @@ from datetime import datetime
 async def print_info(data, query):
     res = []
     for post in data:
-        query.replace(' ', '')
+        clean_query = query.replace(' ', '')
         res.append(f"Время: {datetime.fromtimestamp(post['date']).strftime('%Y-%m-%d %H:%M:%S')}   "
                    f"Автор: https://vk.com/id{post['from_id']}    "
-                   f"Ссылка на комментарий: https://vk.com/wall{post['owner_id']}?q={query}&w=wall{post['owner_id']}_{post['id']}_r{post['id']}    "
+                   f"Ссылка на комментарий: https://vk.com/wall{post['owner_id']}?q={clean_query}&w=wall{post['owner_id']}_{post['id']}_r{post['id']}    "
                    f""
                    f"---------------------------------------------------------------------------------------------------")
 
